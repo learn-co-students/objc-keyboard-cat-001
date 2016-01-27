3febfec3e6e53a23053add864be40c8ce623f5c2
joshmotley <joshkmotley@gmail.com>
2016-01-26 23:02:28 -0500

    complete NSNotificationCenter exercises

--- KeyboardCat/ViewController.m
+++ KeyboardCat/ViewController.m
@@ -9,8 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *textViewBottom;
+
+
+
 
 @end
 
@@ -19,9 +22,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    
+    
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    
+
+    
 
     UIImage *catImage = [UIImage animatedImageNamed:@"keyboard-cat-" duration:3];
     self.imageView.image = catImage;
+    
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(hideKeyboard)];
+    [self.view addGestureRecognizer:tap];
 }
+
+-(void)keyboardWillShow:(NSNotification *)notification{
+    NSDictionary *userInfo = [notification userInfo];
+    CGSize keyboardSize = [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue].size;
+    self.textViewBottom.constant = keyboardSize.height +10;
+
+    
+}
+-(void)keyboardWillHide:(NSNotification *)notification{
+    self.textViewBottom.constant = 20;
+    
+    
+}
+
+-(void)hideKeyboard{
+    [self.view endEditing:YES];
+    NSLog(@"Tap");
+}
+
+
+
 
 @end
